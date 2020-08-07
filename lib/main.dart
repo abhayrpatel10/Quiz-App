12904181c66dbdbcfea1ea2ce6b97b9d7cbc4f08
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-08-07 19:28:53 +0530

    hello world app

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MyQuizApp());
+}
+
+class MyQuizApp extends StatelessWidget{
+  @override
+  build(BuildContext context){
+      return MaterialApp(home:Text('Hello World'),);
+  }
+}
