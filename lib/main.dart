cdf78845329b8b58e4d207d0719b897f7d21912b
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-08-07 21:47:57 +0530

    basic structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
+void main() {
   runApp(MyQuizApp());
 }
 
-class MyQuizApp extends StatelessWidget{
+class MyQuizApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!!!');
+  }
   @override
-  build(BuildContext context){
-      return MaterialApp(home:Text('Hello World'),);
+  build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text('Question 1'),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+          ],
+        ),
+      ),
+    );
   }
 }
