2d7429bfc29c9d778845142a51011f980469d3e0
abhayrpatel10 <abhayrpatel10@gmail.com>
2020-08-08 22:54:12 +0530

    state changes ;

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,30 @@ void main() {
   runApp(MyQuizApp());
 }
 
-class MyQuizApp extends StatelessWidget {
+class MyQuizApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyQuizAppState();
+  }
+}
+
+class MyQuizAppState extends State<MyQuizApp> {
+
+  var index=0;
+
+  var questions=[
+    'What is your favourite color?',
+    'cats or dogs',
+    'summer or winter'
+  ];
 
   void answerQuestion(){
     print('Answer chosen!!!');
+    setState(() {
+      index=index+1;
+    });
+    //index++;
   }
   @override
   build(BuildContext context) {
@@ -18,9 +38,11 @@ class MyQuizApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('Question 1'),
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            Text(questions[index]),
+            RaisedButton(child: Text('Answer 1'), onPressed: ()=>print('AnswerOne Chosen !!')),
+            RaisedButton(child: Text('Answer 1'), onPressed: (){
+              print('Answer 2 chosen');
+            }),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
           ],
